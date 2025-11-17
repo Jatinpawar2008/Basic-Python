b318e70992c554aad937c26342ef7472cb204e74
Jatinpawar2008 <jatinpawar2008p@gmail.com>
2025-11-17 21:45:34 +0530

    Add string manipulation methods in String.py

---
+++ String.py
@@ -0,0 +1,14 @@
+# user input in python
+S = input ("Enter string : ")
+print(S)
+
+#method of string 
+print(S.capitalize()) #first letter of these string is capital
+
+print(S.upper()) #every letter of string is capital
+
+print(S.lower()) #every letter of string is small
+
+print(S.title()) # first letter of every word in string is capital
+
+print(len(S)) # it is count no. of letters in string 
